6ff22a6719b5d1e8da929d3bb31048623bba0164
RubySrivastava <ruby.srivas1984@gmail.com>
2021-05-31 09:37:20 +0000

    udate region name and add env file

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,6 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if os.path.exists("env.py"):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -184,7 +186,7 @@ if 'USE_AWS' in os.environ:
     }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'rubysrivastava-boutique-ado'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
